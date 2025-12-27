882edc6b37d7c28563bb2407f1f181dfe1699e78
Prowin7 <iec2022118@iiita.ac.in>
2025-12-27 11:59:33 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
 
   const currencySymbol = '$'
-  const backendUrl = 'http://localhost:4000'
+  const backendUrl = import.meta.env.VITE_API_URL
 
   const [doctors, setDoctors] = useState([])
   const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
