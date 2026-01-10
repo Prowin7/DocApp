bd8f022130387a090bb5ddedf35d5f03cd462b56
Prowin7 <iec2022118@iiita.ac.in>
2026-01-10 13:14:35 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,7 +11,7 @@ const AdminContextProvider = (props) => {
   const [appointments, setAppointments] = useState([])
   const [dashData, setDashData] = useState(false)
 
-  const backendUrl = 'http://localhost:4000'
+  const backendUrl = 'https://docapp-backend-g13f.onrender.com'
 
   const getAllDoctors = async () => {
 
